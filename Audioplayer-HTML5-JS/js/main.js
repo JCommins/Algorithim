c803cd59fc9e2ef5dfe46f82d04ce7bf19164ab0
JUSTIN COMMINS <justin.commins.2018@mumail.ie>
2018-07-02 13:31:46 +0100

    Audioplayer-HTML5-CSS-JS

--- Audioplayer-HTML5-JS/js/main.js
+++ Audioplayer-HTML5-JS/js/main.js
@@ -1,80 +1,80 @@
-var audio; 
+var audio;
 
 //Hide pause button initilially
-$('#pause').hide(); 
-
+$('#pause').hide();
+	
 //Initialize Audio
 //pass in the first song -> 1st list item
-initAudio($('#playlist li:first-child')); 
-
+initAudio($('#playlist li:first-child'));
 //main function -> initializer
 function initAudio(element){
-	//takes in an element i.e. the first song -> first <li> tag
-	//get  an attribute --> attribute to get is song
-	var song = element.attr('song'); 
-	//get the title --> the actual text
-	var title = element.text(); 
-	var cover = element.attr('cover'); 
-	var artist = element.attr('artist'); 
-	
-	//Create audio object & get the song
-	audio = new Audio ('media/' + song); 
+	//takes in an element i.e. the first song -> first <li> tagvar audio; 
+	//main function -> initializer
+	var song = element.attr('song');
+    var title = element.text();
+    var cover = element.attr('cover');
+    var artist = element.attr('artist');
 
-	//if there's no time i.e. if the song hasn't started yet
+	//Create audio object & get the song
+	audio = new Audio('media/' + song);
+	
+	//If there's no time i.e. if the song hasn't started yet
 	if(!audio.currentTime){
 		//then the display will be 0.00
-		$('#duration').html('0.00'); 
+		$('#duration').html('0.00');
 	}
-
-	//generate the artist and title of song
-	//.text lets us put something there in text & thta's going to be the title variable 
-	//which holds element.text() --> getting it from the html; 
+	/*
+	generate the artist and title of song
+	.text lets us put something there in text & thta's going to be the title variable 
+	which holds element.text() --> getting it from the html; 
+	*/
 	$('#audio-player .title').text(title);
-	$('#audio-player .artist').text(artist);
+    $('#audio-player .artist').text(artist);
 	
 	//Insert cover 
 	//cover variabe at line 13 --> comes from list item
-	$('img.cover').attr('src','img/covers/' +cover);
+	$('img.cover').attr('src','img/covers/' + cover);
 	
-	//want to remove the active class from the last song and put it in this 
-	
-	$('playlist li').removeClass('active'); 
-	element.addClass('active'); 
+	$('#playlist li').removeClass('active');
+    element.addClass('active');
 }
 
-//Play Button 
+
+//Play Button
 $('#play').click(function(){
-	audio.play(); //use the API -> created the new object audio -> have the song, its pointing toward the media directory
-	$('#play').hide(); 
-	$('#pause').show(); 
-	$('#duration').fadeIn(400); 
-	showDuration(); //make so the timer goes along with song
+	audio.play();
+	$('#play').hide();
+	$('#pause').show();
+	$('#duration').fadeIn(400);
+	showDuration();
 });
 
-//Pause Button 
+//Pause Button
 $('#pause').click(function(){
-	audio.pause(); //use the API -> created the new object audio -> have the song, its pointing toward the media directory
-	$('#pause').hide(); 
-	$('#play').show(); 
+	audio.pause();
+	$('#pause').hide();
+	$('#play').show();
 });
-
-//no stop.audio -> can pause song and reset to zero -> stop functionality
+	
 //Stop Button
 $('#stop').click(function(){
-	audio.pause(); 
-	audio.currentTime = 0; 
-	$('#pause').hide(); 
+	audio.pause();		
+	audio.currentTime = 0;
+	$('#pause').hide();
 	$('#play').show();
-	$('#duration').fadeOut(400); 
+	$('#duration').fadeOut(400);
 });
 
 //Next Button
 $('#next').click(function(){
     audio.pause();
-    var next = $('#playlist li.active').next();
+	//#playlist li.active -> gives the current  song -> we want the next 
+    var next = $('#playlist li.active').next(); 
+	//if we're on the last song and click the next, then go to the first song
     if (next.length == 0) {
         next = $('#playlist li:first-child');
     }
+	//will then re-initilialize the audio
     initAudio(next);
 	audio.play();
 	showDuration();
@@ -83,7 +83,8 @@ $('#next').click(function(){
 //Prev Button
 $('#prev').click(function(){
     audio.pause();
-    var prev = $('#playlist li.active').prev();
+    var prev = $('#playlist li.active').prev(); 
+	//if we're on the first song and click previous, it will go to the last song
     if (prev.length == 0) {
         prev = $('#playlist li:last-child');
     }
@@ -107,24 +108,22 @@ $('#playlist li').click(function () {
 $('#volume').change(function(){
 	audio.volume = parseFloat(this.value / 10);
 });
-
-//time duration 
+	
+//Time Duration
 function showDuration(){
 	$(audio).bind('timeupdate', function(){
-		//Get Hours & Minutes
-		var secs = parseInt(audio.currentTime % 60); 
-		var mins = parseInt((audio.currentTime/ 60) % 60); 
-		//Add 0 if less than 10
-		if(secs < 10){
-			secs = '0' + secs;
+		//Get hours and minutes
+		var s = parseInt(audio.currentTime % 60);
+		var m = parseInt((audio.currentTime / 60) % 60);
+		//Add 0 if seconds less than 10
+		if (s < 10) {
+			s = '0' + s;
 		}
-		$('#duration').html(mins + '.' +secs); 
-		var value = 0; 
-		if(audio.currentTime > 0){
-			value = Math.floor((100 / audio.duration) * audio.currentTime); 
+		$('#duration').html(m + '.' + s);	
+		var value = 0;
+		if (audio.currentTime > 0) {
+			value = Math.floor((100 / audio.duration) * audio.currentTime);
 		}
-		//calculating the position of the progress bar when it plays
-		//dynamically setting the width property of the progress div
-		$('#progress').css('width', value+'%'); 
-	}); 
+		$('#progress').css('width',value+'%');
+	});
 }
