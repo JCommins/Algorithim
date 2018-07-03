442f74d19b7c0e45bbfb76fac1ff9b73603275ae
JCommins <35921701+JCommins@users.noreply.github.com>
2018-07-03 14:43:59 +0100

    Update add_SQL.sql

--- PHP-MySQL-Quiz/add_SQL.sql
+++ PHP-MySQL-Quiz/add_SQL.sql
@@ -1,22 +1,7 @@
--- phpMyAdmin SQL Dump
--- version 4.7.7
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Generation Time: Apr 15, 2018 at 05:34 PM
--- Server version: 10.1.30-MariaDB
--- PHP Version: 7.2.1
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `Quiz`
