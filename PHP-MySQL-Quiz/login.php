d6cc0041f15418ba4520fe5edffe621a07e3ea26
JCommins <35921701+JCommins@users.noreply.github.com>
2018-07-03 14:49:31 +0100

    Update login.php

--- PHP-MySQL-Quiz/login.php
+++ PHP-MySQL-Quiz/login.php
@@ -1,5 +1,3 @@
-<?php //reference: http://www.allphptricks.com/ ?>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,25 +8,26 @@
 </head>
 <body>
 <?php
-	require('database.php');
-	session_start();
+    require('database.php');
+    session_start();
     // If form submitted, insert values into the database.
     if (isset($_POST['username'])){
-		
-		$username = stripslashes($_REQUEST['username']); // removes backslashes
-		$username = mysqli_real_escape_string($mysqli,$username); //escapes special characters in a string
-		$password = stripslashes($_REQUEST['password']);
-		$password = mysqli_real_escape_string($mysqli,$password);
+	$username = stripslashes($_REQUEST['username']); // removes backslashes
+	$username = mysqli_real_escape_string($mysqli,$username); //escapes special characters in a string
+	$password = stripslashes($_REQUEST['password']);
+	$password = mysqli_real_escape_string($mysqli,$password);
 		
 	//Checking is user existing in the database or not
         $query = "SELECT * FROM `users` WHERE username='$username' and password='".md5($password)."'";
-		$result = mysqli_query($mysqli,$query) or die(mysql_error());
-		$rows = mysqli_num_rows($result);
+	$result = mysqli_query($mysqli,$query) or die(mysql_error());
+	$rows = mysqli_num_rows($result);
+	    
         if($rows==1){
-			$_SESSION['username'] = $username;
-			header("Location: dashboard.php"); // Redirect user to index.php
-            }else{
-				echo "<div class='form'><h3>Username/password is incorrect.</h3><br/>Click here to <a href='login.php'>Login</a></div>";
+		$_SESSION['username'] = $username;
+		header("Location: dashboard.php"); // Redirect user to index.php
+        }else{
+	
+        echo "<div class='form'><h3>Username/password is incorrect.</h3><br/>Click here to <a href='login.php'>Login</a></div>";
 				}
     }else{
 ?>
